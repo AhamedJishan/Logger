bf4b6c4a8526ce5dc8793028a600a29771edf552
Jishan Ahamed <jjahamed33@gmail.com>
2024-05-18 23:10:57 +0530

    added file output

--- include/Logger.h
+++ include/Logger.h
@@ -1,6 +1,10 @@
 #pragma once
 
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
+#include <cstdio>
+
 
 enum LogPriority
 {
@@ -16,12 +20,21 @@ class Logger
 {
 private:
 	LogPriority logPriority = LogPriority::Info;
-
+	std::FILE* file = 0;
 
 public:
 	static void SetPriority(LogPriority newPriority)
 	{
 		get_instance().logPriority = newPriority;
+	}
+
+	static void EnableFileOutput()
+	{
+		get_instance().enable_file_output("log/logs.txt");
+	}
+	static void EnableFileOutput(const char* filepath)
+	{
+		get_instance().enable_file_output(filepath);
 	}
 
 	template<typename... Args>
@@ -37,12 +50,12 @@ public:
 	template<typename... Args>
 	static void Info(const char* msg, Args... args)
 	{
-		get_instance().log(LogPriority::Info, "[Info]\t", msg, args...);
+		get_instance().log(LogPriority::Info, "[Info ]\t", msg, args...);
 	}
 	template<typename... Args>
 	static void Warning(const char* msg, Args... args)
 	{
-		get_instance().log(LogPriority::Warning, "[Warn]\t", msg, args...);
+		get_instance().log(LogPriority::Warning, "[Warn ]\t", msg, args...);
 	}
 	template<typename... Args>
 	static void Error(const char* msg, Args... args)
@@ -52,14 +65,17 @@ public:
 	template<typename... Args>
 	static void Critical(const char* msg, Args... args)
 	{
-		get_instance().log(LogPriority::Critical, "[Crit]\t", msg, args...);
+		get_instance().log(LogPriority::Critical, "[Crit ]\t", msg, args...);
 	}
 
 private:
 	Logger() = default;
 	~Logger()
 	{
-
+		if (file)
+		{
+			close_file();
+		}
 	}
 
 	// Copy and Assignment constructors are deleted as only one instance is required
@@ -72,6 +88,25 @@ private:
 		return instance;
 	}
 
+	void enable_file_output(const char* filepath)
+	{
+		if (file)
+		{
+			close_file();
+		}
+
+		file = std::fopen(filepath, "w");
+
+		if (!file)
+		{
+			Error("Failed to open the log's file");
+		}
+	}
+	void close_file()
+	{
+		std::fclose(file);
+	}
+
 	template<typename... Args>
 	void log(LogPriority priority, const char* logPriorityString, const char* msg, Args... args)
 	{
@@ -80,6 +115,13 @@ private:
 			printf(logPriorityString);
 			printf(msg, args...);
 			printf("\n");
+
+			if (file)
+			{
+				fprintf(file, logPriorityString);
+				fprintf(file, msg, args...);
+				fprintf(file, "\n");
+			}
 		}
 	}
 };
