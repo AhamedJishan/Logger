714bc20c01151a71ccd928afd81b39293d9e42e3
Jishan Ahamed <jjahamed33@gmail.com>
2024-05-18 21:49:39 +0530

    Basic logging and basic test

--- include/Logger.h
+++ include/Logger.h
@@ -1,1 +1,85 @@
 #pragma once
+
+#include <stdio.h>
+
+enum LogPriority
+{
+	Trace,
+	Debug,
+	Info,
+	Warning,
+	Error,
+	Critical
+};
+
+class Logger
+{
+private:
+	LogPriority logPriority = LogPriority::Info;
+
+
+public:
+	static void SetPriority(LogPriority newPriority)
+	{
+		get_instance().logPriority = newPriority;
+	}
+
+	template<typename... Args>
+	static void Trace(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Trace, "[Trace]\t", msg, args...);
+	}
+	template<typename... Args>
+	static void Debug(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Debug, "[Debug]\t", msg, args...);
+	}
+	template<typename... Args>
+	static void Info(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Info, "[Info]\t", msg, args...);
+	}
+	template<typename... Args>
+	static void Warning(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Warning, "[Warn]\t", msg, args...);
+	}
+	template<typename... Args>
+	static void Error(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Error, "[Error]\t", msg, args...);
+	}
+	template<typename... Args>
+	static void Critical(const char* msg, Args... args)
+	{
+		get_instance().log(LogPriority::Critical, "[Crit]\t", msg, args...);
+	}
+
+private:
+	Logger() = default;
+	~Logger()
+	{
+
+	}
+
+	// Copy and Assignment constructors are deleted as only one instance is required
+	Logger(const Logger&) = delete;
+	Logger operator=(const Logger&) = delete;
+
+	static Logger& get_instance()
+	{
+		static Logger instance;
+		return instance;
+	}
+
+	template<typename... Args>
+	void log(LogPriority priority, const char* logPriorityString, const char* msg, Args... args)
+	{
+		if (priority >= logPriority)
+		{
+			printf(logPriorityString);
+			printf(msg, args...);
+			printf("\n");
+		}
+	}
+};
---
+++ src/LoggingTest.cpp
@@ -0,0 +1,14 @@
+#include "Logger.h"
+
+int main()
+{
+	Logger::SetPriority(Debug);
+
+	Logger::Trace("Logging int: %d", 1);
+	Logger::Debug("Logging int: %d", 2);
+	Logger::Info("Logging int: %d", 3);
+	Logger::Warning("Logging int: %d", 4);
+	Logger::Error("Logging int: %d", 5);
+	Logger::Critical("Logging int: %d", 6);
+	return 0;
+}
